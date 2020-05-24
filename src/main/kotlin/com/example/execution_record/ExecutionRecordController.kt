73f0232e8fddf687131d7926ebbcea406433aebb
Sigeno <sigenogoro@users.noreply.github.com>
2020-05-24 13:16:14 +0900

    Make the foundation of the dashboard

--- src/main/kotlin/com/example/execution_record/ExecutionRecordController.kt
+++ src/main/kotlin/com/example/execution_record/ExecutionRecordController.kt
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import javax.annotation.PostConstruct
+
 
 
 @Controller
